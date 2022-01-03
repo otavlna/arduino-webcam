ced818245ed94bdbf681a42d7941c10a22f66092
Ota Vlna <otavlna@protonmail.ch>
2022-01-03 01:48:29 +0100

    set up code editor

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Editor v-model:code="code" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Editor from './components/Editor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Editor
+  },
+  data () {
+    return {
+      code: null
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ frontend/src/components/Editor.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-ace-editor
+    :value="code"
+    @update:value="$emit('update:code', $event)"
+    @init="editorInit"
+    lang="c_cpp"
+    theme="chrome"
+    style="height: 300px"
+    ref="ace"
+  />
+</template>
+
+<script>
+import { VAceEditor } from 'vue3-ace-editor'
+import 'ace-builds/src-noconflict/mode-c_cpp'
+import 'ace-builds/src-noconflict/theme-chrome'
+
+export default {
+  name: 'Editor',
+  components: {
+    VAceEditor
+  },
+  props: ['code'],
+  emits: ['update:code'],
+  methods: {
+    editorInit () {
+      console.log('editor init')
+    }
+  },
+  mounted () {
+    console.log(this.$refs.ace)
+  }
+}
+</script>
