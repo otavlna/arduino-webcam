2e6216472363e25567fef1510f6278bbc479394e
Ota Vlna <otavlna@protonmail.ch>
2022-03-16 23:39:00 +0100

    implemented reconnect logic

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <header class="py-4">
-    <div class="lg:mx-20 mx-5">
-      <h1 class="text-primary"><font-awesome-icon icon="camera" /> Arduino Webcam</h1>
+    <div class="lg:mx-20 mx-5 flex">
+      <h1 class="text-primary">
+        <font-awesome-icon icon="camera" />
+        Arduino Webcam
+      </h1>
+      <div class="ml-5">
+        <span v-if="websocketReady" class="text-green-600">
+          <font-awesome-icon icon="wifi" />
+          Connected
+        </span>
+        <span v-else class="text-red-600">
+          <font-awesome-icon icon="exclamation-circle"/>
+          Trying to reconnect...
+        </span>
+      </div>
     </div>
   </header>
-  <main class=" flex flex-col items-stretch main pb-2">
+  <main class="flex flex-col items-stretch main pb-2">
     <div class="flex h-full">
       <div class="flex-left h-full">
-        <Editor class="h-full" v-model:code="code" v-model:lang="lang" @upload="upload" />
+        <Editor
+          class="h-full"
+          v-model:code="code"
+          v-model:lang="lang"
+          @upload="upload"
+        />
       </div>
       <div class="flex-right h-full flex flex-col">
         <Log :messages="messages" class="bg-white overflow-y-auto flex-1" />
@@ -18,57 +36,74 @@
 </template>
 
 <script>
-import Editor from './components/Editor.vue'
-import Log from './components/Log.vue'
-import Player from './components/Player.vue'
+import Editor from "./components/Editor.vue";
+import Log from "./components/Log.vue";
+import Player from "./components/Player.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Editor,
     Log,
-    Player
+    Player,
   },
-  data () {
+  data() {
     return {
-      code: '',
-      lang: 'wiring',
-      messages: ['Waiting for input...'],
-      ws: null
-    }
+      code: "",
+      lang: "wiring",
+      messages: ["Waiting for input..."],
+      ws: null,
+      websocketReady: false
+    };
   },
   methods: {
-    upload () {
-      this.ws.send(JSON.stringify({ code: this.code, lang: this.lang }))
-    },
-    formatMessage (message) {
-      const formatted = message.replace(/\n/g, '<br />')
-      return formatted
-    }
-  },
-  created () {
-    this.ws = new WebSocket('ws://localhost:1337')
+    establishConnection() {
+      this.ws = new WebSocket("ws://localhost:1337");
 
-    this.ws.addEventListener('open', (event) => {
-      this.ws.addEventListener('message', (event) => {
-        const message = JSON.parse(event.data)
-        if (message.type === 'log') {
+      this.ws.addEventListener("open", (event) => {
+        console.log(this.ws);
+        console.log("socket connected");
+        this.websocketReady = true
+      });
+
+      this.ws.addEventListener("message", (event) => {
+        const message = JSON.parse(event.data);
+        if (message.type === "log") {
           if (message.stderr) {
-            this.messages.push(this.formatMessage(message.stderr))
+            this.messages.push(this.formatMessage(message.stderr));
           }
         }
-      })
-    })
+      });
 
-    this.ws.addEventListener('message', (event) => {
-      console.log('Message from server ', event.data)
-    })
+      this.ws.addEventListener("error", (event) => {
+        console.log("socket encountered error");
+        this.ws.close();
+      });
+
+      this.ws.addEventListener("close", (event) => {
+        console.log("socket closed, trying to reconnect");
+        this.websocketReady = false
+        setTimeout(() => {
+          this.establishConnection();
+        }, 1000);
+      });
+    },
+    upload() {
+      this.ws.send(JSON.stringify({ code: this.code, lang: this.lang }));
+    },
+    formatMessage(message) {
+      const formatted = message.replace(/\n/g, "<br />");
+      return formatted;
+    },
+  },
+  created() {
+    this.establishConnection();
   }
-}
+};
 </script>
 
 <style scoped>
 .main {
-  height: calc(100% - 56px)
+  height: calc(100% - 56px);
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 import './main.css'
 import LoadScript from 'vue-plugin-load-script'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUpload, faCamera, faFileUpload, faFileDownload } from '@fortawesome/free-solid-svg-icons'
+import { faUpload, faCamera, faFileUpload, faFileDownload, faWifi, faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add([faUpload, faCamera, faFileUpload, faFileDownload])
+library.add([faUpload, faCamera, faFileUpload, faFileDownload, faWifi, faExclamationCircle])
 
 const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
 
