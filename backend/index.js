d11fcd2e2e9aedf26dd9afaca20cd7102556450b
Ota Vlna <otavlna@protonmail.ch>
2022-03-17 22:11:09 +0100

    backend refactor

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,52 @@
 import { WebSocketServer } from 'ws';
 import { exec } from 'child_process';
 import fs from 'fs';
+import { promisify } from 'util'
+const execPromise = promisify(exec)
 
 const wss = new WebSocketServer({ port: 1337 });
 
+let device = ''
+
 wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  ws.on('message', async function message(data) {
+    const request = JSON.parse(data)
 
-    const response = JSON.parse(data)
-    console.log(response)
-
-    if (response.lang === 'c') {
-      console.log('kód je v céčku')
-      fs.writeFile("tmp_c/code.c", response.code, function (err) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log("Soubor ulozen!");
-        exec('avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o tmp_c/code.o tmp_c/code.c && avr-gcc -mmcu=atmega328p tmp_c/code.o -o tmp_c/code && avr-objcopy -O ihex -R .eeprom tmp_c/code tmp_c/code.hex && avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:tmp_c/code.hex', (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-          }
-          ws.send(JSON.stringify({ type: 'log', stderr }))
-        });
-        });
+    const { stdout, stderr } = await execPromise('ls /dev | grep ttyUSB')
+    if (stderr) {
+      console.error(stderr)
     }
-    else if (response.lang === 'wiring') {
-      console.log('kód je ve wiringu')
-      fs.writeFile("../programy-wiring/code/code.ino", response.code, function (err) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log("Soubor ulozen!");
-        exec('export ARDUINO_DIR=../arduino-1.8.16 && export export ARDMK_DIR=./Makefile && export AVR_TOOLS_DIR=/usr/include && ARDUINO_DIR="../arduino-1.8.16" ARDMK_DIR="./Makefile"  AVR_TOOLS_DIR="/usr/include" make && avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:build-uno/code.hex', { cwd: '../programy-wiring/code/' }, (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-          }
-          ws.send(JSON.stringify({ type: 'log', stdout, stderr }))
-        });
-      });
+    device = stdout.trim()
+
+    if (request.lang === 'c') {
+      processC(request, ws)
+    }
+    else if (request.lang === 'wiring') {
+      processWiring(request, ws)
     }
   });
 });
+
+function processC(request, ws) {
+  fs.writeFile("code-c/code.c", request.code, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    const command = `avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o code.o code.c && avr-gcc -mmcu=atmega328p code.o -o code && avr-objcopy -O ihex -R .eeprom code code.hex && avrdude -F -V -c arduino -p ATMEGA328P -P /dev/${device} -b 115200 -U flash:w:code.hex`
+    exec(command, { cwd: 'code-c' }, (error, stdout, stderr) => {
+      ws.send(JSON.stringify({ type: 'log', stderr }))
+    });
+  });
+}
+
+function processWiring(request, ws) {
+  fs.writeFile("code-wiring/code.ino", request.code, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    const command = `export ARDUINO_DIR=../../arduino-1.8.16 && export ARDMK_DIR=./Makefile && export AVR_TOOLS_DIR=/usr/include && avrdude -F -V -c arduino -p ATMEGA328P -P /dev/${device} -b 115200 -U flash:w:build-uno/code-wiring.hex`
+    exec(command, { cwd: 'code-wiring' }, (error, stdout, stderr) => {
+      ws.send(JSON.stringify({ type: 'log', stdout, stderr }))
+    });
+  });
+}
