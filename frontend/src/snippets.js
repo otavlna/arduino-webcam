5235ec320040c94b65e1b70d9d4163e834ea1388
Ota Vlna <otavlna@protonmail.ch>
2022-04-10 21:07:52 +0200

    added snippets

--- frontend/src/snippets.js
+++ frontend/src/snippets.js
@@ -13,15 +13,152 @@ void loop() {
     {
       name: 'Blink',
       code:
-`void setup() {
-  pinMode(13, OUTPUT);
+`constexpr int LED_PIN = 7;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(13, HIGH);
+    digitalWrite(LED_PIN, HIGH);
     delay(1000);
-    digitalWrite(13, LOW);
+    digitalWrite(LED_PIN, LOW);
     delay(1000);
+}`
+    },
+    {
+      name: 'Button Pull-Up',
+      code:
+`constexpr int BTN_PIN = 2;
+constexpr int LED_PIN = 7;
+
+void setup() {
+    pinMode(BTN_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
+}
+void loop() {
+  int sensorVal = digitalRead(BTN_PIN);
+
+  if (sensorVal == HIGH) {
+
+    digitalWrite(LED_PIN, LOW);
+
+  } else {
+
+    digitalWrite(LED_PIN, HIGH);
+
+  }
+}`
+    },
+    {
+      name: 'Traffic Lights',
+      code:
+`constexpr int RED_LED = 3;
+constexpr int YELLOW_LED = 4;
+constexpr int GREEN_LED = 5;
+
+void setup() {
+    pinMode(RED_LED, OUTPUT);
+    pinMode(YELLOW_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(RED_LED, HIGH);
+  delay(3000);
+  digitalWrite(YELLOW_LED, HIGH);
+  delay(1000);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(GREEN_LED, HIGH);
+  delay(3000);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, HIGH);
+  delay(1000);
+  digitalWrite(YELLOW_LED, LOW);
+}`
+    },
+    {
+      name: 'RGB LED',
+      code:
+`constexpr int RED_LED = 9;
+constexpr int GREEN_LED = 10;
+constexpr int BLUE_LED = 11;
+
+void setup() {
+    pinMode(RED_LED, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(BLUE_LED, OUTPUT);
+}
+
+void loop() {
+  for(int i = 0; i < 256; i++) {
+      analogWrite(RED_LED, i);
+      delay(4);
+  }
+  for(int i = 0; i < 256; i++) {
+      analogWrite(GREEN_LED, i);
+      delay(4);
+  }
+  for(int i = 0; i < 256; i++) {
+      analogWrite(BLUE_LED, i);
+      delay(4);
+  }
+  for(int i = 255; i >= 0; i--) {
+      analogWrite(RED_LED, i);
+      delay(4);
+  }
+  for(int i = 255; i >= 0; i--) {
+      analogWrite(GREEN_LED, i);
+      delay(4);
+  }
+  for(int i = 255; i >= 0; i--) {
+      analogWrite(BLUE_LED, i);
+      delay(4);
+  }
+}`
+    },
+    {
+      name: 'Photoresistor',
+      code:
+`constexpr int LED_1 = 3;
+constexpr int LED_2 = 4;
+constexpr int LED_3 = 5;
+constexpr int LED_4 = 6;
+constexpr int LED_5 = 7;
+
+void setup() {
+    pinMode(LED_1, OUTPUT);
+    pinMode(LED_2, OUTPUT);
+    pinMode(LED_3, OUTPUT);
+    pinMode(LED_4, OUTPUT);
+    pinMode(LED_5, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+    digitalWrite(LED_5, LOW);
+    
+    int reading = analogRead(A0);
+    
+    if(reading > 50) {
+      digitalWrite(LED_1, HIGH);
+    }
+    if(reading > 100) {
+      digitalWrite(LED_2, HIGH);
+    }
+    if(reading > 200) {
+      digitalWrite(LED_3, HIGH);
+    }
+    if(reading > 350) {
+      digitalWrite(LED_4, HIGH);
+    }
+    if(reading > 600) {
+      digitalWrite(LED_5, HIGH);
+    }
 }`
     }
   ],
@@ -45,24 +182,16 @@ int main(void) {
 
 int main(void)
 {
-  /*Set to one the fifth bit of DDRB to one
-  **Set digital pin 13 to output mode */
-  DDRB |= _BV(DDB5);
+  DDRD |= _BV(DDD7);
 
   while (1)
   {
-    /*Set to one the fifth bit of PORTB to one
-    **Set to HIGH the pin 13 */
-    PORTB |= _BV(PORTB5);
+    PORTD |= _BV(PORTD7);
 
-    /*Wait 3000 ms */
     _delay_ms(MS_DELAY);
 
-    /*Set to zero the fifth bit of PORTB
-    **Set to LOW the pin 13 */
-    PORTB &= ~_BV(PORTB5);
+    PORTD &= ~_BV(PORTD7);
 
-    /*Wait 3000 ms */
     _delay_ms(MS_DELAY);
   }
 }`
