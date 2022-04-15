162a18596c7ba8b8e25bdba00bc917a3d9b95346
Ota Vlna <otavlna@protonmail.ch>
2022-04-15 21:39:25 +0200

    cleanup

--- frontend/src/components/Player.vue
+++ frontend/src/components/Player.vue
@@ -10,29 +10,12 @@ export default {
   name: 'Player',
   data () {
     return {
-      player: null,
-      streamWidth: 1280,
-      streamHeight: 720,
-      windowWidth: 0,
-      windowHeight: 0
+      player: null
     }
   },
   computed: {
-    sizeCoef () {
-      if (this.windowWidth < 600) {
-        return 0.25
-      } else if (this.windowWidth < 900) {
-        return 0.3
-      } else if (this.windowWidth < 1200) {
-        return 0.4
-      } else if (this.windowWidth < 1500) {
-        return 0.5
-      } else return 0.65
-    },
     options () {
       return {
-        width: this.streamWidth * this.sizeCoef,
-        height: this.streamHeight * this.sizeCoef,
         channel: 'arduinowebcam',
         parent: ['localhost', 'sudety.ch', 'www.sudety.ch']
       }
@@ -42,7 +25,6 @@ export default {
     startStream () {
       this.$loadScript('twitch.js')
         .then(() => {
-          // you might need to pass an ID string as a first argument..
           this.player = new window.Twitch.Player(
             this.$refs.twitchVideo,
             this.options
@@ -56,8 +38,6 @@ export default {
     }
   },
   mounted () {
-    this.windowHeight = window.innerHeight
-    this.windowWidth = window.innerWidth
     this.startStream()
   }
 }
